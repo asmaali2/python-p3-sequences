007a2cea8368c41cfc660671b425f21d51ba8e59
asmaali2 <asma.maalim7@gmail.com>
2023-12-14 14:16:48 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    size=[0,1]
+    while len(size)<length:
+        next_number= size[-1] + size[-2]
+        size.append(next_number)
+    print(size[:length])
     pass
+print_fibonacci(9)
